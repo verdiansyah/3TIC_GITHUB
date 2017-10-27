d9036c0cf1034a3d742e9eaf997e6a737dfb5eb2
verdiansyah <verdiansyah15ti@mahasiswa.pcr.ac.id>
2017-10-27 09:53:27 +0700

    Test GitHub

---
+++ Verdi_GitHub/GitHub1/CobaGitHub.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class CobaGitHub
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Selamat datang di web GitHub Verdi!"
+    End Sub
+
+End Class
